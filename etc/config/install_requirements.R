6087e9e6ff0c1ddfa1a60128ac2bcead4e1617df
yanisaspic <yasloudj@u-bordeaux.fr>
2024-09-20 19:06:31 +0200

    architecture done; need to check re-runs

--- etc/config/install_requirements.R
+++ etc/config/install_requirements.R
@@ -8,10 +8,10 @@ CRAN <- "http://cran.us.r-project.org"
 #___________________________________________________________________________CRAN
 if (!require("aricode")) install_version("aricode", version="1.0.3", repos=CRAN, dependencies=T)
 if (!require("arules")) install_version("arules", version="1.7.7", repos=CRAN, dependencies=T)
-if (!require("BiocManager")) install_version("BiocManager", version="1.30.22", repos=CRAN, dependencies=T)
+if (!require("dbplyr")) install_version("dbplyr", version="2.3.4", repos=CRAN, dependencies=T)
 if (!require("dplyr")) install_version("dplyr", version="1.1.4", repos=CRAN, dependencies=T)
 if (!require("egg")) install_version("egg", version="0.4.5", repos=CRAN, dependencies=T)
-if (!require("entropy")) install_version("entropy", version="1.3.1", repos=CRAN, dependencies=T)
+if (!require("ggh4x")) install_version("ggh4x", version="0.2.8", repos=CRAN, dependencies=T)
 if (!require("ggplot2")) install_version("ggplot2", version="3.5.1", repos=CRAN, dependencies=T)
 if (!require("ggplotify")) install_version("ggplotify", version="0.1.2", repos=CRAN, dependencies=T)
 if (!require("ggVennDiagram")) install_version("ggVennDiagram", version="1.5.0", repos=CRAN, dependencies=T)
@@ -19,38 +19,62 @@ if (!require("glue")) install_version("glue", version="1.7.0", repos=CRAN, depen
 if (!require("gridExtra")) install_version("gridExtra", version="2.3", repos=CRAN, dependencies=T)
 if (!require("igraph")) install_version("igraph", version="1.6.0", repos=CRAN, dependencies=T)
 if (!require("Matrix")) install_version("Matrix", version="1.6.5", repos=CRAN, dependencies=T)
+if (!require("matrixStats")) install_version("matrixStats", version="1.3.0", repos=CRAN, dependencies=T)
 if (!require("openxlsx")) install_version("openxlsx", version="4.2.5.2", repos=CRAN, dependencies=T)
-if (!require("qpdf")) install_version("qpdf", version="1.3.2", repos=CRAN, dependencies=T)
+if (!require("patchwork")) install_version("patchwork", version="1.2.0", repos=CRAN, dependencies=T)
+if (!require("qpdf")) install_version("qpdf", version="1.3.3", repos=CRAN, dependencies=T)
 if (!require("RColorBrewer")) install_version("RColorBrewer", version="1.1.3", repos=CRAN, dependencies=T)
+if (!require("readxl")) install_version("readxl", version="1.4.3", repos=CRAN, dependencies=T)
 if (!require("reshape2")) install_version("reshape2", version="1.4.4", repos=CRAN, dependencies=T)
-if (!require("rlang")) install_version("rlang", version="1.1.3", repos=CRAN, dependencies=T)
+if (!require("rlang")) install_version("rlang", version="1.1.4", repos=CRAN, dependencies=T)
+if (!require("scales")) install_version("scales", version="1.3.0", repos=CRAN, dependencies=T)
 if (!require("SCpubr")) install_version("SCpubr", version="2.0.2", repos=CRAN, dependencies=T)
 if (!require("Seurat")) install_version("Seurat", version="5.0.1", repos=CRAN, dependencies=T)
 if (!require("SeuratObject")) install_version("SeuratObject", version="5.0.1", repos=CRAN, dependencies=T)
 if (!require("sp")) install_version("sp", version="2.1.2", repos=CRAN, dependencies=T)
-
+if (!require("tidytree")) install_version("tidytree", version="0.4.6", repos=CRAN, dependencies=T)
 
 #___________________________________________________________________Bioconductor
-install(version="3.17")
+if (!require("BiocManager")) install_version("BiocManager", version="1.30.23", repos=CRAN, dependencies=T)
+BiocManager::install(version="3.18")
+
+if (!require("Biobase")) install("Biobase", dependencies=T, update=F)
+# packageVersion("Biobase") = 2.62.0
+if (!require("BiocFileCache")) install("BiocFileCache", dependencies=T, update=F)
+# packageVersion("BiocFileCache") = 2.8.0
 if (!require("BiocGenerics")) install("BiocGenerics", dependencies=T, update=F)
+# packageVersion("BiocGenerics") = 0.48.1
 if (!require("GenomeInfoDb")) install("GenomeInfoDb", dependencies=T, update=F)
+# packageVersion("GenomeInfoDb") = 1.36.4
 if (!require("GenomicRanges")) install("GenomicRanges", dependencies=T, update=F)
+# packageVersion("GenomicRanges") = 1.52.1
+if (!require("ggtree")) {install("YuLab-SMU/treedataverse", dependencies=T, update=F)}
+# packageVersion("ggtree") = 3.10.1
 if (!require("IRanges")) install("IRanges", dependencies=T, update=F)
+# packageVersion("IRanges") = 2.34.1
 if (!require("MatrixGenerics")) install("MatrixGenerics", dependencies=T, update=F)
+# packageVersion("MatrixGenerics") = 1.14.0
 if (!require("S4Vectors")) install("S4Vectors", dependencies=T, update=F)
+# packageVersion("S4Vectors") = 0.40.2
 if (!require("scater")) install("scater", dependencies=T, update=F)
+# packageVersion("scater") = 1.28.0
 if (!require("scuttle")) install("scuttle", dependencies=T, update=F)
+# packageVersion("scuttle") = 1.10.3
 if (!require("SingleCellExperiment")) install("SingleCellExperiment", dependencies=T, update=F)
+# packageVersion("SingleCellExperiment") = 1.22.0
 if (!require("SummarizedExperiment")) install("SummarizedExperiment", dependencies=T, update=F)
+# packageVersion("SummarizedExperiment") = 1.30.2
 if (!require("TMExplorer")) install("TMExplorer", dependencies=T, update=F)
-
-install(version="3.18")
-if (!require("ggtree") | !require("tidytree")) {install("YuLab-SMU/treedataverse", dependencies=T, update=F)}
-
+# packageVersion("TMExplorer") = 0.4.6
 
 #______________________________________________________Bitbucket, Github, Gitlab
 if (!require("densitycut")) install_bitbucket("jerry00/densitycut_dev", dependencies=T)
+# packageVersion("densityCut") = 0.0.1
 if (!require("monocle3")) install_github('cole-trapnell-lab/monocle3', dependencies=T)
+# packageVersion("monocle3") = 1.3.4
 if (!require("SeuratWrappers")) install_github('satijalab/seurat-wrappers', dependencies=T)
+# packageVersion("SeuratWrappers") = 0.3.1
 if (!require("SHARP")) install_github("shibiaowan/SHARP", dependencies=T)
+# packageVersion("SHARP") = 1.1.0
 if (!require("SPARSim")) install_gitlab("sysbiobig/sparsim", depencencies=T)
+# packageVersion("SPARSim") = 0.9.5
--- run/setup.sh
+++ run/setup.sh
@@ -2,13 +2,7 @@
 #
 #	Run this script to set-up the datasets used in the paper.
 #
-#	2024/05/23 @yanisaspic
-
-module load r/4.3.1
-module load python/3.9
-
-CONFIG_DIR="./etc/config"
-# Rscript $CONFIG_DIR/install_requirements.R
+#	2024/09/20 @yanisaspic
 
 SETUP_DIR="./etc/source"
 chmod +x $SETUP_DIR/download.sh
